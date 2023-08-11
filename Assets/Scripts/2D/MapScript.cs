Denormos/Worlds

commit f534c7b78d50784b5a50d0900737d867e78eab4a

Added #308
Toolbar disappears while using brush

Assets/Scripts/2D/MapScript.cs
@@ -18,6 +18,7 @@ public class MapScript : MonoBehaviour
     public Material DrainageMaterial;
 
     public UnityEvent ActivatedBrush;
+    public UnityEvent DeactivatedBrush;
 
     private bool _isDraggingMap = false;
     private Vector2 _beginDragPosition;
@@ -311,6 +312,7 @@ public class MapScript : MonoBehaviour
         if (pointerData.button == PointerEventData.InputButton.Left)
         {
             Manager.ActivateEditorBrush(false);
+            DeactivatedBrush.Invoke();
         }
     }
 

commit 713d9e730ec55bc444f30e2d913659f639751d59

Fixed bug caused by f534c7b7

Assets/Scripts/2D/MapScript.cs
@@ -311,8 +311,10 @@ public class MapScript : MonoBehaviour
 
         if (pointerData.button == PointerEventData.InputButton.Left)
         {
-            Manager.ActivateEditorBrush(false);
-            DeactivatedBrush.Invoke();
+            if(Manager.GameMode == GameMode.Editor){
+                Manager.ActivateEditorBrush(false);
+                DeactivatedBrush.Invoke();
+            }
         }
     }
 